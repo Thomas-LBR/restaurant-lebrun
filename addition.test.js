457b0ae70b2ac167442665086822b4c478cfa33a
Thomas Lebrun <thomas.lebrun7210@gmail.com>
2026-01-14 15:17:52 +0100

    Exercice 2 : calcul de l'addition avec tests Jest

--- addition.test.js
+++ addition.test.js
@@ -1,5 +1,5 @@
 const calculerAddition = require("./addition");
 
 test("calculerAddition(15, 5) donne 20", () => {
-  expect(calculerAddition(15, 6)).toBe(20);
+  expect(calculerAddition(15, 5)).toBe(20);
 });
